36c7f506fbad5dfffc6113ba62fe7a718ac4b3d4
hadley <h.wickham@gmail.com>
2014-03-06 10:36:56 -0800

    Make points more visually prominent

--- server.R
+++ server.R
@@ -10,9 +10,9 @@ shinyServer(function(input, output, session) {
   })
 
   output$plot1 <- renderPlot({
-    par(mar=c(5.1, 4.1, 0, 2.1))
-    plot(selectedData(), col=clusters()$cluster)
-    points(clusters()$centers, pch=4, cex=2)
+    par(mar=c(5.1, 4.1, 0, 1))
+    plot(selectedData(), col=clusters()$cluster, pch = 20, cex = 3)
+    points(clusters()$centers, pch=4, cex=4, lwd = 4)
   })
 
 })
