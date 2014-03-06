a472925b0c263580f7437759b540a7d118e0d04c
hadley <h.wickham@gmail.com>
2014-03-06 10:39:37 -0800

    Use consistent spacing

--- server.R
+++ server.R
@@ -5,7 +5,7 @@ shinyServer(function(input, output, session) {
 
   # Combine the selected variables into a new data frame
   selectedData <- reactive({
-    iris[,c(input$xcol, input$ycol)]
+    iris[, c(input$xcol, input$ycol)]
   })
 
   clusters <- reactive({
@@ -13,9 +13,9 @@ shinyServer(function(input, output, session) {
   })
 
   output$plot1 <- renderPlot({
-    par(mar=c(5.1, 4.1, 0, 1))
-    plot(selectedData(), col=clusters()$cluster, pch = 20, cex = 3)
-    points(clusters()$centers, pch=4, cex=4, lwd = 4)
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(), col = clusters()$cluster, pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
   })
 
 })
