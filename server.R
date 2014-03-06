2deacf0c951ec2a540a216b1a759cc905af39d62
hadley <h.wickham@gmail.com>
2014-03-06 10:38:32 -0800

    Use colour brewer set1 palette

--- server.R
+++ server.R
@@ -1,3 +1,6 @@
+palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", 
+  "#A65628", "#F781BF", "#999999"))
+
 shinyServer(function(input, output, session) {
 
   # Combine the selected variables into a new data frame
