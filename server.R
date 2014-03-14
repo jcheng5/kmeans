b32d3d287239362d2cb7299cf489aa813c33471d
Joe Cheng <joe@rstudio.com>
2014-03-13 22:16:02 -0700

    Code cleanup

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
-palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", 
-  "#A65628", "#F781BF", "#999999"))
+palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
 shinyServer(function(input, output, session) {
 
@@ -14,7 +14,9 @@ shinyServer(function(input, output, session) {
 
   output$plot1 <- renderPlot({
     par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(), col = clusters()$cluster, pch = 20, cex = 3)
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
     points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
   })
 
--- ui.R
+++ ui.R
@@ -2,8 +2,10 @@ shinyUI(pageWithSidebar(
   headerPanel('Iris k-means clustering'),
   sidebarPanel(
     selectInput('xcol', 'X Variable', names(iris)),
-    selectInput('ycol', 'Y Variable', names(iris), selected=names(iris)[[2]]),
-    numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
+    selectInput('ycol', 'Y Variable', names(iris),
+                selected=names(iris)[[2]]),
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 9)
   ),
   mainPanel(
     plotOutput('plot1', height=600)
